1aab5c2ff6e09e2e40229e6ee254eb0e0ba51dc2
bbodine1 <bbodine1@gmail.com>
2022-02-01 16:05:13 -0600

    3 files modified, 1 added

--- src/app/shared/components/topnav/topnav.component.ts
+++ src/app/shared/components/topnav/topnav.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import {
+  DxTabPanelModule,
+  DxCheckBoxModule,
+  DxTemplateModule,
+} from 'devextreme-angular';
+
+import { Company, Service } from './topnav.service';
+
 @Component({
   selector: 'mrl-topnav',
   templateUrl: './topnav.component.html',
-  styleUrls: ['./topnav.component.scss']
+  styleUrls: ['./topnav.component.scss'],
 })
 export class TopnavComponent implements OnInit {
-
   @Input() title: string;
 
   constructor() {
     this.title = '';
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
